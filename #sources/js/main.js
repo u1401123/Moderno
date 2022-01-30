a04ba68c84e6f7da261ec21c18381f98762f6b67
u1401123 <igor_mokrushin@mail.ru>
2022-01-30 18:30:33 +0300

    добавлени блок search

--- #sources/js/main.js
+++ #sources/js/main.js
@@ -1,6 +1,7 @@
 //подключение кусков кода в файлах .js
 //=====================================================================================================
 //@prepros-append jq-start.js
+//@prepros-append partials/ibg.js
 //@prepros-append jq-functions.js
 //@prepros-append jq-end.js
 //@prepros-append partials/preloader.js
--- js/main.js
+++ js/main.js
@@ -24,6 +24,20 @@ $(function ($) {
     var act = "touchstart";
   }
 
+function ibg() {
+  $.each($('.ibg'), function (index, val) {
+    if ($(this).find('img').length > 0) {
+      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+    }
+  });
+}
+ibg();
+
+$(document).on('click touchstart', function (e) {
+  if (!$(e.target).is(".select *")) {
+    $('.select').removeClass('active');
+  };
+});
 //=====================================================================================
 //Плавная прокрутка до определенного в href места на странице c помощью класса goto
 //======================================================================================
