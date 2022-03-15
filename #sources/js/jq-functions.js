1af3f710251781414d8f9c5d2caf9bbc72838122
u1401123 <igor_mokrushin@mail.ru>
2022-03-15 09:28:34 +0300

    Добавлена страница contacts

--- #sources/js/jq-functions.js
+++ #sources/js/jq-functions.js
@@ -50,4 +50,22 @@ $('.icon-menu').click(function (event) {
 
 //==================================================================================
 
+
+
+//==================================================================================
+// tabs
+//==================================================================================
+$('body').on('click', '.tab__navitem', function (event) {
+  var eq = $(this).index();
+  if ($(this).hasClass('parent')) {
+    var eq = $(this).parent().index();
+  }
+  if (!$(this).hasClass('active')) {
+    $(this).closest('.tabs').find('.tab__navitem',).removeClass('active');
+    $(this).addClass('active');
+    $(this).closest('.tabs').find('.tab__item').removeClass('active').eq(eq).addClass('active');
+  }
+});
+
+//===========================================================================
 var mixer = mixitup('.products__row');
